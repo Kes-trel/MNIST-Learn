12d458603bf39cd2bac5a5b302f7e8974c5dcaa5
Kes <74243456+Kes-trel@users.noreply.github.com>
2021-06-09 19:22:34 +0100

    Update TensorFlow_MNIST.py

--- TensorFlow_MNIST.py
+++ TensorFlow_MNIST.py
@@ -3,3 +3,24 @@ import tensorflow as tf
 
 import tensorflow_datasets as tfds
 
+# as_supervised=True will load the dataset in a 2-tuple structure (input, target) 
+mnist_dataset, mnist_info = tfds.load(name='mnist', with_info=True, as_supervised=True)
+
+mnist_train, mnist_test = mnist_dataset['train'], mnist_dataset['test']
+
+# by default, TF has training and testing datasets, but no validation sets - define validation sets
+num_validation_samples = 0.1 * mnist_info.splits['train'].num_examples
+
+num_validation_samples = tf.cast(num_validation_samples, tf.int64)
+
+num_test_samples = mnist_info.splits['test'].num_examples
+
+num_test_samples = tf.cast(num_test_samples, tf.int64)
+
+def scale(image, label):
+    image = tf.cast(image, tf.float32)
+    # inputs are 0 to 255, divide each element by 255 and all elements will be between 0 and 1 
+    image /= 255.
+
+    return image, label
+
