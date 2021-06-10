490bf544f7e4c2f532fa299c0054c16042156dfa
Kes <74243456+Kes-trel@users.noreply.github.com>
2021-06-10 10:23:26 +0100

    functioning trainer

--- TensorFlow_MNIST.py
+++ TensorFlow_MNIST.py
@@ -7,14 +7,19 @@ from help_text import *
 
 
 #inputs from streamlit app
+activation_functions_list = ["deserialize", "elu", "exponential", "gelu", "get", "hard_sigmoid", "linear", "relu", "selu", "serialize", "sigmoid", "softmax", "softplus", "softsign", "swish", "tanh"]
 
+model_optimizer_list = ["Adadelta", "Adagrad", "Adam", "Adamax", "Ftrl", "Nadam", "Optimizer", "RMSprop", "SGD"]
+
+model_loss_list = ["KLD","MAE","MAPE","MSE","MSLE","binary_crossentropy","categorical_crossentropy","categorical_hinge","cosine_similarity","deserialize","get","hinge","huber","kl_divergence","kld","kullback_leibler_divergence","log_cosh","logcosh","mae","mape","mean_absolute_error","mean_absolute_percentage_error","mean_squared_error","mean_squared_logarithmic_error","mse","msle","poisson","serialize","sparse_categorical_crossentropy","squared_hinge"]
 
 BUFFER_SIZE = st.sidebar.number_input("Buffer Size", value=100, min_value=1, step=100, format="%i" , help=buffer_size_help)
-
 BATCH_SIZE = st.sidebar.number_input("Batch Size", value=100, min_value=1, step=100, format="%i" , help=batch_size_help)
-
 hidden_layer_size = st.sidebar.number_input("Hidden Layer Unit Size", value=50, min_value=1, step=10, format="%i" , help=hidden_layer_help)
-
+activation_function = st.sidebar.selectbox("Select Activation Function", activation_functions_list, index=7, help=activation_function_help)
+model_optimizer = st.sidebar.selectbox("Select Model Optimiser Class", model_optimizer_list, index=2, help=model_optimizer_help)
+model_loss = st.sidebar.selectbox("Select Model Loss", model_loss_list, index=28, help=model_loss_help)
+NUM_EPOCHS = int(st.sidebar.number_input("Number of epochs", value=5, min_value=1, step=1, help=epochs_help))
 
 # BUFFER_SIZE = 1000
 # BATCH_SIZE = 100
@@ -22,9 +27,7 @@ hidden_layer_size = st.sidebar.number_input("Hidden Layer Unit Size", value=50, 
 # activation_function = "relu"
 # model_optimizer = "adam"
 # model_loss = "sparse_categorical_crossentropy"
-# NUM_EPOCHS = int(st.sidebar.number_input("Number of epochs", min_value=1, step=1, help="Don't go crazy here"))
-
-st.write(type(BATCH_SIZE))
+# NUM_EPOCHS = 5
 
 # as_supervised=True will load the dataset in a 2-tuple structure (input, target) 
 mnist_dataset, mnist_info = tfds.load(name='mnist', with_info=True, as_supervised=True)
@@ -33,11 +36,8 @@ mnist_train, mnist_test = mnist_dataset['train'], mnist_dataset['test']
 
 # by default, TF has training and testing datasets, but no validation sets - define validation sets
 num_validation_samples = 0.1 * mnist_info.splits['train'].num_examples
-
 num_validation_samples = tf.cast(num_validation_samples, tf.int64)
-
 num_test_samples = mnist_info.splits['test'].num_examples
-
 num_test_samples = tf.cast(num_test_samples, tf.int64)
 
 def scale(image, label):
@@ -59,11 +59,8 @@ train_data = shuffled_train_and_validation_data.skip(num_validation_samples)
 # BATCH_SIZE = 100
 
 train_data = train_data.batch(BATCH_SIZE)
-
 validation_data = validation_data.batch(num_validation_samples)
-
 test_data = test_data.batch(num_test_samples)
-
 validation_inputs, validation_targets = next(iter(validation_data))
 
 # MODEL
@@ -97,13 +94,13 @@ model.compile(optimizer=model_optimizer, loss=model_loss, metrics=["accuracy"])
 
 history = model.fit(train_data, epochs=NUM_EPOCHS, validation_data=(validation_inputs, validation_targets), verbose =0)
 
+st.write(history.history)
+
 df = pd.DataFrame.from_dict(history.history)
 df.columns = ["Loss", "Accuracy %", "Validation Loss", "Validation Accuracy %"]
 df["Accuracy %"] = df["Accuracy %"] * 100
 df["Validation Accuracy %"] = df["Validation Accuracy %"] * 100
 df.insert(loc=0, column="Epoch", value=df.index+1)
-
-st.write(history.history)
 
 st.table(df)
 
--- help_text.py
+++ help_text.py
@@ -18,5 +18,70 @@ A bigger batch size results in slower training. We are taking advantage of batch
 
 hidden_layer_help = """
 To keep simple here we will use same hidden layer unit size for all hidden layers. If you think this is too simple you should not be using this site my friend.
-Try 200 or 500 and so on... The validation accuracy is significantly higher (as the algorithm with 50 hidden units was too simple of a model). Naturally, it takes the algorithm much longer to train (unless early stopping is triggered too soon).
+* Try 200 or 500 and so on... The validation accuracy is significantly higher (as the algorithm with 50 hidden units was too simple of a model). Naturally, it takes the algorithm much longer to train (unless early stopping is triggered too soon).
+"""
+
+activation_function_help = """
+Adjust the activations from 'relu' to 'sigmoid'.
+
+Generally, we should reach an inferior solution. That is because relu 'cleans' the noise in the data (think about it - if a value is negative, relu filters it out, while if it is positive, it takes it into account). For the MNIST dataset, we care only about the intensely black and white parts in the images of the digits, so such filtering proves beneficial.
+
+The sigmoid does not filter the signals as well as relu, but still reaches a respectable result (around 95%).
+
+For Tensor Flow - built-in activation functions see:
+https://www.tensorflow.org/api_docs/python/tf/keras/activations
+"""
+
+model_optimizer_help = """
+Built-in optimizer classes.
+
+* Adadelta: Optimizer that implements the Adadelta algorithm.
+* Adagrad: Optimizer that implements the Adagrad algorithm.
+* Adam: Optimizer that implements the Adam algorithm.
+* Adamax: Optimizer that implements the Adamax algorithm.
+* Ftrl: Optimizer that implements the FTRL algorithm.
+* Nadam: Optimizer that implements the NAdam algorithm.
+* Optimizer: Base class for Keras optimizers.
+* RMSprop: Optimizer that implements the RMSprop algorithm.
+* SGD: Gradient descent (with momentum) optimizer.
+
+https://www.tensorflow.org/api_docs/python/tf/keras/optimizers
+"""
+
+model_loss_help = """
+Functions
+* KLD(...): Computes Kullback-Leibler divergence loss between y_true and y_pred.
+* MAE(...): Computes the mean absolute error between labels and predictions.
+* MAPE(...): Computes the mean absolute percentage error between y_true and y_pred.
+* MSE(...): Computes the mean squared error between labels and predictions.
+* MSLE(...): Computes the mean squared logarithmic error between y_true and y_pred.
+* binary_crossentropy(...): Computes the binary crossentropy loss.
+* categorical_crossentropy(...): Computes the categorical crossentropy loss.
+* categorical_hinge(...): Computes the categorical hinge loss between y_true and y_pred.
+* cosine_similarity(...): Computes the cosine similarity between labels and predictions.
+* deserialize(...): Deserializes a serialized loss class/function instance.
+* get(...): Retrieves a Keras loss as a function/Loss class instance.
+* hinge(...): Computes the hinge loss between y_true and y_pred.
+* huber(...): Computes Huber loss value.
+* kl_divergence(...): Computes Kullback-Leibler divergence loss between y_true and y_pred.
+* kld(...): Computes Kullback-Leibler divergence loss between y_true and y_pred.
+* kullback_leibler_divergence(...): Computes Kullback-Leibler divergence loss between y_true and y_pred.
+* log_cosh(...): Logarithm of the hyperbolic cosine of the prediction error.
+* logcosh(...): Logarithm of the hyperbolic cosine of the prediction error.
+* mae(...): Computes the mean absolute error between labels and predictions.
+* mape(...): Computes the mean absolute percentage error between y_true and y_pred.
+* mean_absolute_error(...): Computes the mean absolute error between labels and predictions.
+* mean_absolute_percentage_error(...): Computes the mean absolute percentage error between y_true and y_pred.
+* mean_squared_error(...): Computes the mean squared error between labels and predictions.
+* mean_squared_logarithmic_error(...): Computes the mean squared logarithmic error between y_true and y_pred.
+* mse(...): Computes the mean squared error between labels and predictions.
+* msle(...): Computes the mean squared logarithmic error between y_true and y_pred.
+* poisson(...): Computes the Poisson loss between y_true and y_pred.
+* serialize(...): Serializes loss function or Loss instance.
+* sparse_categorical_crossentropy(...): Computes the sparse categorical crossentropy loss.
+* squared_hinge(...): Computes the squared hinge loss between y_true and y_pred.
+"""
+
+epochs_help = """
+An epoch indicates the number of passes of the entire training dataset the machine learning algorithm has completed. Determining how many epochs a model should run to train is based on many parameters related to both the data itself and the goal of the model.
 """
